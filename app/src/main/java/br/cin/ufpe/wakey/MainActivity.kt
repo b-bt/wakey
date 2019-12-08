041eea442b9050c3959b1183185e56483eb9a1d7
Bruno Barbosa <brunobt94@gmail.com>
2019-12-08 19:28:14 -0300

    Added map to MainActivity
    Added a map fragment and centered it at CIn UFPE

--- app/src/main/java/br/cin/ufpe/wakey/MainActivity.kt
+++ app/src/main/java/br/cin/ufpe/wakey/MainActivity.kt
@@ -2,11 +2,37 @@ package br.cin.ufpe.wakey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Move the camera to a certaing location and zoom in
+        val location = LatLng(-8.0557, -34.9516)
+        val newPosition = CameraPosition.Builder()
+            .target(location)
+            .zoom(15f)
+            .build()
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(newPosition))
     }
 }
