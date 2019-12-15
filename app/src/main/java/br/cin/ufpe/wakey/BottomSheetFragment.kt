e00d2de96571e65a74b55ee721ac8b44b1c42f07
Bruno Barbosa <brunobt94@gmail.com>
2019-12-15 17:03:29 -0300

    Created wakey list in bottom sheet

--- app/src/main/java/br/cin/ufpe/wakey/BottomSheetFragment.kt
+++ app/src/main/java/br/cin/ufpe/wakey/BottomSheetFragment.kt
@@ -1,22 +1,33 @@
 package br.cin.ufpe.wakey
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 
 class BottomSheetFragment : Fragment() {
+
+    private lateinit var listView: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        listView = activity!!.findViewById<ListView>(R.id.listview)
+
+        val data = arrayOf("Home", "Work", "School")
+        val adapter = RecentWakeyAdapter(activity!!.applicationContext, data)
+        listView.adapter = adapter
     }
 
 }
