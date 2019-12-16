aa546a69fbde761d60594cb265cd1f46262d5230
Bruno Barbosa <brunobt94@gmail.com>
2019-12-15 23:30:35 -0300

    Adjusting radius on WakeyDetailActivity

--- app/src/main/java/br/cin/ufpe/wakey/WakeyDetailActivity.kt
+++ app/src/main/java/br/cin/ufpe/wakey/WakeyDetailActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,11 +15,12 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.activity_wakey_detail.*
 
-class WakeyDetailActivity : AppCompatActivity(), OnMapReadyCallback {
+class WakeyDetailActivity : AppCompatActivity(), OnMapReadyCallback, SeekBar.OnSeekBarChangeListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var markerCenter: Marker
     private lateinit var radiusCircle: Circle
+    private var selectedRadius: Double = 100.0
 
     companion object {
         const val EXTRA_WAKEY: String = "wakey"
@@ -36,23 +38,29 @@ class WakeyDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wakey_detail)
 
-        // Check if editing or creating a wakey
+        // Configure view according to if editing or creating a wakey
         val wakey = intent.extras?.getString(EXTRA_WAKEY)
         if (wakey != null) {
             setTitle(R.string.title_edit_activity)
             nameEditText.setText(wakey)
             saveBtn.text = resources.getString(R.string.button_save)
+            selectedRadius = 700.0
         } else {
             setTitle(R.string.title_create_activity)
             deleteBtn.visibility = View.GONE
             saveBtn.text = resources.getString(R.string.button_create)
+            selectedRadius = (resources.getInteger(R.integer.default_radius) + 1) * 100.0
         }
+        radiusTextView.text = "${selectedRadius.toInt()}m"
 
         // Config map fragment
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.mapView) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        // Config SeekBar
+        radiusSeekBar.progress = ((selectedRadius / 100.0) - 1).toInt()
+        radiusSeekBar.setOnSeekBarChangeListener(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -70,7 +78,7 @@ class WakeyDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         // Add radius circle around marker
         val circleOptions = CircleOptions()
             .center(mMap.cameraPosition.target)
-            .radius(100.0)
+            .radius(selectedRadius)
             .strokeColor(ContextCompat.getColor(this, R.color.radiusCircleBorder))
             .fillColor(ContextCompat.getColor(this, R.color.radiusCircleFill))
         radiusCircle = mMap.addCircle(circleOptions)
@@ -84,5 +92,20 @@ class WakeyDetailActivity : AppCompatActivity(), OnMapReadyCallback {
             markerCenter.position = position
             radiusCircle.center = position
         }
+    }
+
+    override fun onStartTrackingTouch(p0: SeekBar?) {
+        // Not implemented
+    }
+
+    override fun onStopTrackingTouch(p0: SeekBar?) {
+        // Not implemented
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        var newRadius = (progress + 1) * 100.0
+        selectedRadius = newRadius
+        radiusCircle.radius = newRadius
+        radiusTextView.text = "${newRadius.toInt()}m"
     }
 }
